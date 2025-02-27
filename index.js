411ca621f23ec60a00a7fc8aeebda0e20812eb4e
Krutarth Kadia <202201475@daiict.ac.in>
2025-02-28 00:58:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose'); // ✅ Import Mongoose
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const expressSession = require('express-session');
 const flash = require('connect-flash');
 
